195bcc32c578799776d640e6c6f2717ec90a5e67
Prince Reyes <krayzerprince@gmail.com>
2024-03-31 15:39:07 +0800

    fix: Correct Word in TaskStatus

--- src/tasks/task.model.ts
+++ src/tasks/task.model.ts
@@ -1,4 +1,4 @@
-export interface Task  {
+export interface Task {
     id: string,
     title: string,
     description: string,
@@ -6,7 +6,7 @@ export interface Task  {
 }
 
 export enum TaskStatus {
-    OPEN = 'OPEN',
+    OPEN = 'OPENS',
     IN_PROGRESS = 'IN_PROGRESS',
     DONE = 'DONE'
 }
